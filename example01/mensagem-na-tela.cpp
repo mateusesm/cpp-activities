ec8eac3643789d28d982bb38b13cc8e813c3119d
Mateus Macedo <74915414+mateusesm@users.noreply.github.com>
2020-11-30 10:04:44 -0300

    On-screen message in C++
    Simple program to make some information appear on the screen, in C ++

---
+++ example01/mensagem-na-tela.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  cout << "Olá, mundo!" << endl;
+
+  cout << endl;
+
+	cout << "Nome completo" << endl;
+	cout << "Rua, Avenida, Bairro, Número" << endl;
+    cout << "Cidade, Estado" << endl;
+    cout << "CEP" << endl;
+}
